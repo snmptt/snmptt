b6b0f6b124323391e520053ffefce8cd0b234bbe
Alex Burger <alex_b@users.sourceforge.net>
2002-09-04 15:14:06 +0000

    Added snmptt-init.d file for /etc/rc.d/init.d

---
+++ snmptt/snmptt-init.d
@@ -0,0 +1,76 @@
+#!/bin/bash
+# init file for snmptt
+# Alex Burger - 8/29/02
+#
+# processname: /usr/sbin/snmptt
+# pidfile: /var/run/snmptt
+
+# source function library
+. /etc/init.d/functions
+
+OPTIONS="--daemon"
+RETVAL=0
+prog="snmptt"
+
+start() {
+	echo -n $"Starting $prog: "
+        daemon /usr/sbin/snmptt $OPTIONS
+	RETVAL=$?
+	echo
+	touch /var/lock/subsys/snmptt
+	return $RETVAL
+}
+
+stop() {
+	echo -n $"Stopping $prog: "
+	killproc /usr/sbin/snmptt 2>/dev/null
+	RETVAL=$?
+	echo
+	rm -f /var/lock/subsys/snmptt
+	return $RETVAL
+}
+
+reload(){
+        echo -n $"Reloading config file: "
+        killproc snmptt -HUP
+        RETVAL=$?
+        echo
+        return $RETVAL
+}
+
+restart(){
+	stop
+	start
+}
+
+condrestart(){
+    [ -e /var/lock/subsys/snmptt ] && restart
+    return 0
+}
+
+case "$1" in
+  start)
+	start
+	;;
+  stop)
+	stop
+	;;
+  restart)
+	restart
+        ;;
+  reload)
+	reload
+        ;;
+  condrestart)
+	condrestart
+	;;
+  status)
+        status snmptt
+	RETVAL=$?
+        ;;
+  *)
+	echo $"Usage: $0 {start|stop|restart|condrestart|reload}"
+	RETVAL=1
+esac
+
+exit $RETVAL
