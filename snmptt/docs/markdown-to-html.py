b5c41489f068c569ce6b65dec643739393118c25
Alex Burger <alex.burger@gmail.com>
2021-03-21 09:12:30 -0400

    Add markdown converter

---
+++ snmptt/docs/markdown-to-html.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+import markdown
+
+# sudo pip3 install markdown
+
+with open(sys.argv[1], 'r') as f:
+    text = f.read()
+    html = markdown.markdown(text)
+    
+print(html)
